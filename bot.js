b927eb01fcad872e80ed143598c076119160bd4d
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-07-22 22:01:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '>'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`>help | >invite `,"http://twitch.tv/S-F")
+client.user.setGame(`>help | >invite ${client.guilds.size} `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
