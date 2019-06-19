24f9e2f8bbc77d30145fad137a7dbcd6f6735062
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2019-06-19 18:00:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,105 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '>';
-const ownerID = '391665977949028363'
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(`>help | >invite ${client.guilds.size} `,"http://twitch.tv/S-F")
+const prefix = '>'
+const ms = require("ms");
+const ownerID = '523142437397331968'
+client.on("ready", async () => {
+    console.log(`${client.user.username} is now online...`);
+    client.user.setActivity(`${prefix}help ${prefix}invite ${client.guilds.size}`, "http://twitch.tv/S-F")
 });
 
+client.on('message', function(msg) {
+    if(msg.content.startsWith(prefix + 'kick')) {
+      let messagearray = msg.content.split(" ");
+      let Kuser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
+      if(!Kuser) return msg.channel.send("**Sorry, but i didnt find the member**");
+      if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("**You dont have permission : KICK_MEMBERS**");
+      if(Kuser.hasPermission("ADMINISTRATOR")) return msg.channel.send("Sorry, but you cant kick an administrator");
+  
+      let Kembed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setTitle(":eight_pointed_black_star: :heavy_minus_sign: ༺**A member has been kicked**༻ :heavy_minus_sign: :eight_pointed_black_star:")
+      .setDescription(`
+      ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+      **${msg.author}** kicks ${Kuser}
+      ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+      `)
+      .setTimestamp()
 
-client.on('message', message => {
-    if (message.author.bot) return;
+      msg.guild.member(Kuser).kick(Kreason);
 
+      let Kchannel = msg.guild.channels.find('name', "logs");
+      if(!Kchannel) return ;
 
-    let args = message.content.slice(prefix.length).trim().split(' ');
-    let cmd = args.shift().toLowerCase();
-
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
-
-    try{
-        delete require.cache[require.resolve(`./commands/${cmd}.js`)];
-
-        let ops = {
-            ownerID: ownerID
-        }
-
-        let commandFile = require(`./commands/${cmd}.js`)
-        commandFile.run(client, message, args, ops,);
-    } catch(e) {
-        console.log(e.stack);
+      Kchannel.send(Kembed);
+      msg.channel.send('**The member has been kicked**');
+      msg.delete();
     }
-
-
-
 });
 
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', '﹄welcome﹃')
-    if(!channel) return;
-    channel.send(member.user + " has joined the server!");
 
-    let role = member.guild.roles.find('name', '- Member')
-    member.addRole(role);
+client.on("message", function(msg) {
+    let args = msg.content.substring(prefix.length).split(" ");
+    if(!message.channel.guild) return;
+    if(msg.content.startsWith(prefix + "clear")) {
+    if(!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.replay ('لا تملك صلاحية حدف الرسائل');
+    var tfa7;
+          tfa7 = parseInt();
+        
+        msg.channel.fetchMessages({limit: tfa7}).then(messages => msg.channel.bulkDelete(messages)).catch(console.error);
+  
+        message.channel.sendMessage("", {embed: {
+            title: "Done | تــم",
+            color: 0x06DF00,
+            description: "تم مسح الرسائل بنجاح",
+            footer: {
+              text: "Dragons"
+            }
+          }}).then(msg => {msg.delete(3000)});
+                              }
+    
+         
+    });
 
-    member.send("I Hope u enjoy our server <3")
-});
 
-client.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', '﹄welcome﹃')
-    if(!channel) return;
-    channel.send(member.user + " has left the server!")
-})
+    client.on('message', message => {
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return;
+    if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('Sorry, but you dont have permisson : **ADMINISTRATOR**' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    if (!args) return message.reply('**Sorry, But you should type a message**');message.channel.send(`**Confirm**`).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))   
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`**☑ |   The message has been sended to ${message.guild.members.size} member**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new Discord.RichEmbed()
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setThumbnail(client.user.avatarURL)
+    .setTimestamp()
+    .setColor('RANDOM')
+    .setDescription(`
+    ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+    ❤️Server : **${message.guild.name}**
+    ❤️Sender : **${message.author.username}**
+    ❤️Message : **${args}**
+    ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+    `)           
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Brodcast cancled :x:.**`).then(m => m.delete(5000));
+    })
+    })
+    }
+    });
 
-
-client.on('ready', () => {
-    console.log('Started!');
-})
-client.login(process.env.BOT_TOKEN);
+    
+    
